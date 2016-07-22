a875131af7941637d9349d833436d3bb95929847
XenRon <xenron@outlook.com>
2016-07-22 01:26:13 +0000

    msg

---
+++ book/Apache.Mesos.Essentials/MonteCarloArea/src/main/java/org/packt/mesos/MonteCarloExecutor.java
@@ -0,0 +1,108 @@
+package org.packt.mesos;
+
+import org.apache.mesos.Executor;
+import org.apache.mesos.ExecutorDriver;
+import org.apache.mesos.MesosExecutorDriver;
+import org.apache.mesos.Protos;
+import org.apache.mesos.protobuf.ByteString;
+
+public class MonteCarloExecutor implements Executor{
+    Expression expression;
+    double xLow;
+    double xHigh;
+    double yLow;
+    double yHigh;
+    int n;
+
+    public MonteCarloExecutor(Expression expression, double xLow, double xHigh, double yLow, double yHigh, int n) {
+        this.expression = expression;
+        this.xLow = xLow;
+        this.xHigh = xHigh;
+        this.yLow = yLow;
+        this.yHigh = yHigh;
+        this.n=n;
+    }
+
+    @Override
+    public void registered(ExecutorDriver executorDriver, Protos.ExecutorInfo executorInfo, Protos.FrameworkInfo frameworkInfo, Protos.SlaveInfo slaveInfo) {
+        System.out.println("Registered an executor on slave " + slaveInfo.getHostname());
+}
+
+    @Override
+    public void reregistered(ExecutorDriver executorDriver, Protos.SlaveInfo slaveInfo) {
+        System.out.println("Re-Registered an executor on slave " + slaveInfo.getHostname());
+    }
+
+    @Override
+    public void disconnected(ExecutorDriver executorDriver) {
+        System.out.println("Re-Disconnected the executor on slave");
+    }
+
+    @Override
+    public void launchTask(final ExecutorDriver executorDriver, final Protos.TaskInfo taskInfo) {
+        System.out.println("Launching task "+taskInfo.getTaskId().getValue());
+        Thread thread = new Thread() {
+            @Override
+            public void run(){
+                //Notify the status as running
+                Protos.TaskStatus status = Protos.TaskStatus.newBuilder()
+                                                   .setTaskId(taskInfo.getTaskId())
+                                                   .setState(Protos.TaskState.TASK_RUNNING)
+                                                   .build();
+                executorDriver.sendStatusUpdate(status);
+                System.out.println("Running task "+taskInfo.getTaskId().getValue());
+                double pointsUnderCurve=0;
+                double totalPoints=0;
+
+                for(double x=xLow;x<=xHigh;x+=(xHigh-xLow)/n){
+                    for (double y=yLow;y<=yHigh;y+=(yHigh-yLow)/n) {
+                        double value=expression.evaluate(x);
+                        if (value >= y) {
+                            pointsUnderCurve++;
+                        }
+                        totalPoints++;
+                    }
+                }
+                double area=(xHigh - xLow)*(yHigh - yLow) * pointsUnderCurve/totalPoints; // Area of Rectangle * fraction of points under curve
+                //Notify the status as finish
+                status = Protos.TaskStatus.newBuilder()
+                                 .setTaskId(taskInfo.getTaskId())
+                                 .setState(Protos.TaskState.TASK_FINISHED)
+                                 .setData(ByteString.copyFrom(Double.toString(area).getBytes()))
+                                 .build();
+                executorDriver.sendStatusUpdate(status);
+                System.out.println("Finished task "+taskInfo.getTaskId().getValue()+ " with area : "+area);
+            }
+        };
+
+        thread.start();
+    }
+
+    @Override
+    public void killTask(ExecutorDriver executorDriver, Protos.TaskID taskID) {
+        System.out.println("Killing task " + taskID);
+    }
+
+    @Override
+    public void frameworkMessage(ExecutorDriver executorDriver, byte[] bytes) {
+    }
+
+    @Override
+    public void shutdown(ExecutorDriver executorDriver) {
+        System.out.println("Shutting down the executor");
+    }
+
+    @Override
+    public void error(ExecutorDriver executorDriver, String s) {
+
+    }
+
+    public static void main(String[] args) {
+        if(args.length<6){
+            System.err.println("Usage: MonteCarloExecutor <Expression> <xLow> <xHigh> <yLow> <yHigh> <Number of Points>");
+        }
+        MesosExecutorDriver driver = new MesosExecutorDriver(new MonteCarloExecutor(Expression.fromString(args[0]),Double.parseDouble(args[1]), Double.parseDouble(args[2]), Double.parseDouble(args[3]), Double.parseDouble(args[4]), Integer.parseInt(args[5])));
+        Protos.Status status = driver.run();
+        System.out.println("Driver exited with status "+status);
+    }
+}
---
+++ book/Apache.Mesos.Essentials/MonteCarloArea/src/main/java/org/packt/mesos/MonteCarloScheduler.java
@@ -0,0 +1,129 @@
+package org.packt.mesos;
+
+import org.apache.mesos.Protos;
+import org.apache.mesos.Scheduler;
+import org.apache.mesos.SchedulerDriver;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MonteCarloScheduler implements Scheduler {
+    private LinkedList<String> tasks;
+    private int numTasks;
+    private int tasksSubmitted;
+    private int tasksCompleted;
+    private double totalArea;
+
+    public MonteCarloScheduler(String[] args, int numTasks){
+        this.numTasks=numTasks;
+        tasks=new LinkedList<String>();
+        double xLow=Double.parseDouble(args[1]);
+        double xHigh=Double.parseDouble(args[2]);
+        double yLow=Double.parseDouble(args[3]);
+        double yHigh=Double.parseDouble(args[4]);
+        double xStep=(xHigh-xLow)/(numTasks/2);
+        double yStep=(yHigh-yLow)/(numTasks/2);
+        for (double x=xLow;x<xHigh;x+=xStep){
+            for (double y=yLow;y<yHigh;y+=yStep) {
+                tasks.add(" \" "+args[0]+" \" "+x+" "+(x+xStep)+" "+y+" "+(y+ yStep)+" "+args[5]);
+            }
+        }
+    }
+
+    @Override
+    public void registered(SchedulerDriver schedulerDriver, Protos.FrameworkID frameworkID, Protos.MasterInfo masterInfo) {
+        System.out.println("Scheduler registered with id " + frameworkID.getValue());
+    }
+
+    @Override
+    public void reregistered(SchedulerDriver schedulerDriver, Protos.MasterInfo masterInfo) {
+        System.out.println("Scheduler re-registered");
+    }
+
+    @Override
+    public void resourceOffers(SchedulerDriver schedulerDriver, List<Protos.Offer> offers) {
+        for (Protos.Offer offer : offers) {
+            if(tasks.size()>0) {
+                tasksSubmitted++;
+                String task = tasks.remove();
+                Protos.TaskID taskID = Protos.TaskID.newBuilder().setValue(String.valueOf(tasksSubmitted)).build();
+                System.out.println("Launching task " + taskID.getValue()+" on slave "+offer.getSlaveId().getValue()+" with "+task);
+                Protos.ExecutorInfo executor = Protos.ExecutorInfo.newBuilder()
+                        .setExecutorId(Protos.ExecutorID.newBuilder().setValue(String.valueOf(tasksSubmitted)))
+                        .setCommand(createCommand(task))
+                        .setName("MonteCarlo Executor (Java)")
+                        .build();
+
+                Protos.TaskInfo taskInfo = Protos.TaskInfo.newBuilder()
+                        .setName("MonteCarloTask-" + taskID.getValue())
+                        .setTaskId(taskID)
+                        .setExecutor(Protos.ExecutorInfo.newBuilder(executor))
+                        .addResources(Protos.Resource.newBuilder()
+                                .setName("cpus")
+                                .setType(Protos.Value.Type.SCALAR)
+                                .setScalar(Protos.Value.Scalar.newBuilder()
+                                        .setValue(1)))
+                        .addResources(Protos.Resource.newBuilder()
+                                .setName("mem")
+                                .setType(Protos.Value.Type.SCALAR)
+                                .setScalar(Protos.Value.Scalar.newBuilder()
+                                        .setValue(128)))
+                        .setSlaveId(offer.getSlaveId())
+                        .build();
+                schedulerDriver.launchTasks(Collections.singletonList(offer.getId()), Collections.singletonList(taskInfo));
+            }
+        }
+    }
+
+    private Protos.CommandInfo.Builder createCommand(String args){
+        return Protos.CommandInfo.newBuilder().setValue("java -cp /vagrant/MonteCarloArea.jar:/usr/share/java/mesos-0.20.1-shaded-protobuf.jar:/vagrant/protobuf-java-2.5.0.jar  -Djava.library.path=/usr/local/lib org.packt.mesos.MonteCarloExecutor "+args);
+    }
+
+    @Override
+    public void offerRescinded(SchedulerDriver schedulerDriver, Protos.OfferID offerID) {
+
+    }
+
+    @Override
+    public void statusUpdate(SchedulerDriver schedulerDriver, Protos.TaskStatus taskStatus) {
+        System.out.println("Status update: task "+taskStatus.getTaskId().getValue()+" state is "+taskStatus.getState());
+        if (taskStatus.getState().equals(Protos.TaskState.TASK_FINISHED)){
+            tasksCompleted++;
+            double area = Double.parseDouble(taskStatus.getData().toStringUtf8());
+            totalArea+=area;
+            System.out.println("Task "+taskStatus.getTaskId().getValue()+" finished with area : "+area);
+        } else {
+            System.out.println("Task "+taskStatus.getTaskId().getValue()+" has message "+taskStatus.getMessage());
+        }
+        if(tasksCompleted==numTasks){
+            System.out.println("Total Area : "+totalArea);
+            schedulerDriver.stop();
+        }
+    }
+
+    @Override
+    public void frameworkMessage(SchedulerDriver schedulerDriver, Protos.ExecutorID executorID, Protos.SlaveID slaveID, byte[] bytes) {
+
+    }
+
+    @Override
+    public void disconnected(SchedulerDriver schedulerDriver) {
+
+    }
+
+    @Override
+    public void slaveLost(SchedulerDriver schedulerDriver, Protos.SlaveID slaveID) {
+
+    }
+
+    @Override
+    public void executorLost(SchedulerDriver schedulerDriver, Protos.ExecutorID executorID, Protos.SlaveID slaveID, int i) {
+
+    }
+
+    @Override
+    public void error(SchedulerDriver schedulerDriver, String message) {
+        System.err.println("Error : "+message);
+    }
+}
